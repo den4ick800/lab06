e684b828478bb59df05f1431ca5472f673c0eee4
den4ick800 <danilka_pr@mail.ru>
2021-05-10 04:26:26 -0700

    f

--- hello_world_application/hello_world.cpp
+++ hello_world_application/hello_world.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
-#include <string>
+
+#include "formatter_ex.h"
+
 int main()
 {
-//create for getting name
-String name_1;
-//ask for name
-std::cout<<"Write your name:";
-std::cin>>name_1;
-//write answer
-std::cout<<"Hello, world!!!"<<name<<std::endl;
-return 0;
+    formatter(std::cout, "hello, world!");
 }
-
